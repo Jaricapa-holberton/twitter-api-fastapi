3a89a0d7919a9327648837807704272e6bc3c71c
Jaime Aricapa <2241@holbertonschool.com>
2021-12-28 15:49:01 -0500

    add main structure for the path operations

--- main.py
+++ main.py
@@ -1,16 +1,140 @@
 # Python
+from typing import Dict, List, Optional
+
 # Pydantic
-# FastAPI
-from fastapi import FastAPI
-
-
 # Models
-from models import User, UserIn, UserOut
-from models import Tweet
+from models import UserBase, User, UserLogin, Tweet
+
+# FastAPI
+from fastapi import FastAPI, status, Path
 
 app = FastAPI()
 
+@app.get('/',
+         summary='Home',
+         status_code=status.HTTP_200_OK)
+def home() -> Dict[str, str]:
+    """Home route.
 
-@app.get(path="/", tags=["Home"])
+    Returns a message indicating that the app is running.
+    """
+
+    return {
+        'message': 'Twitter API is working!',
+    }
+
+
+## Auth
+@app.post(
+    path='/signup',
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary='Register an User',
+    tags=['Users']
+)
+def signup():
+    pass
+
+@app.post(
+    path='/login',
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary='Login an User',
+    tags=['Users']
+)
+def login():
+    pass
+
+## Users
+
+@app.get(
+    path='/users',
+    response_model=List[User],
+    status_code=status.HTTP_200_OK,
+    summary='Show all users',
+    tags=['Users']
+)
+def show_all_users():
+    pass
+
+@app.get(
+    path='/users/{user_id}',
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary='Show an User',
+    tags=['Users']
+)
+def show_an_user():
+    pass
+
+@app.delete(
+    path='/users/{user_id}/delete',
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary='Delete an User',
+    tags=['Users']
+)
+def delete_an_user():
+    pass
+
+@app.put(
+    path='/users/{user_id}/update',
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary='Update an User',
+    tags=['Users']
+)
+def update_an_user():
+    pass
+
+## Tweets
+
+@app.get(
+    path='/',
+    response_model=List[Tweet],
+    status_code=status.HTTP_200_OK,
+    summary='Show all tweets',
+    tags=['Tweets']
+)
 def home():
-    return {"message": "Welcome"}
+    return {'Twitter API': 'Working!'}
+
+@app.post(
+    path='/post',
+    response_model=Tweet,
+    status_code=status.HTTP_201_CREATED,
+    summary='Post a tweet',
+    tags=['Tweets']
+)
+def post():
+    pass
+
+@app.get(
+    path='/tweet/{tweet_id}',
+    response_model=Tweet,
+    status_code=status.HTTP_200_OK,
+    summary='Show a tweet',
+    tags=['Tweets']
+)
+def show_a_tweet():
+    pass
+
+@app.delete(
+    path='/tweet/{tweet_id}',
+    response_model=Tweet,
+    status_code=status.HTTP_200_OK,
+    summary='Delete a tweet',
+    tags=['Tweets']
+)
+def delete_a_tweet():
+    pass
+
+@app.put(
+    path='/tweet/{tweet_id}',
+    response_model=Tweet,
+    status_code=status.HTTP_200_OK,
+    summary='Update a tweet',
+    tags=['Tweets']
+)
+def update_a_tweet():
+    pass
--- models.py
+++ models.py
@@ -4,29 +4,57 @@ from datetime import date, datetime
 from typing import Optional
 
 # Pydantic
-from pydantic import BaseModel
-from pydantic import Field, EmailStr
+from pydantic import BaseModel, Field, EmailStr
+
+class UserBase(BaseModel):
+
+    id: UUID = Field(...,)
+
+    email: EmailStr = Field(...,)
 
 
-class User(BaseModel):
-    user_id: UUID = Field(..., alias="id")
-    email: EmailStr = Field(..., example="Jhon@doe.com")
-    first_name: str = Field(..., min_length=2, max_length=50, example="John")
-    last_name: str = Field(..., min_length=2, max_length=50, example="Doe")
-    birthday: Optional[date] = Field(default=None)
+class User(UserBase):
+
+    first_name: str = Field(...,
+                            title='First name',
+                            min_length=2,
+                            max_length=50,
+                            example='John',)
+
+    last_name: str = Field(...,
+                           title='Last name',
+                           min_length=2,
+                           max_length=50,
+                           example='Doe',)
+
+    birth_date: Optional[date] = Field(default=None,
+                                       title='Birth date',
+                                       example='2021-01-01',)
 
 
-class UserIn(User):
-    password: str = Field(..., min_length=8, max_length=30)
+class UserLogin(UserBase):
 
-
-class UserOut(User):
-    pass
+    password: str = Field(...,
+                          min_length=8,
+                          max_length=64,
+                          example='password',)
 
 
 class Tweet(BaseModel):
-    tweet_id: UUID = Field(..., alias="Tweet id")
-    content: str = Field(..., min_length=2, max_length=240, example="Hello World")
-    created_at: datetime = Field(default=datetime.now())
-    updated_at: Optional[datetime] = Field(default=None)
-    by: UserIn = Field(..., alias="User")
+
+    id: UUID = Field(...)
+
+    content: str = Field(...,
+                         min_length=1,
+                         max_length=256,)
+
+    created_at: datetime = Field(default=datetime.now(),
+                                title='Creation date',
+                                example='2020-01-01T00:00:00Z',)
+
+    updated_at: Optional[datetime] = Field(default=None,
+                                           title='Last update date',
+                                           example='2020-01-01T00:00:00Z',)
+
+    created_by: User = Field(...,
+                             title='User who created the tweet',)
