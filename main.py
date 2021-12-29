ac5b3dfa1996cf5a2c27a9ae383fc797efb1aa8d
Jaime Aricapa <2241@holbertonschool.com>
2021-12-28 19:30:36 -0500

    define singup function

--- main.py
+++ main.py
@@ -1,77 +1,111 @@
 # Python
 from typing import List , Dict
+import json
 
 # Pydantic
 # Models
-from models import UserBase, User, UserLogin, Tweet
+from models import User, UserLogin, UserRegister, Tweet
 
 # FastAPI
-from fastapi import FastAPI, status, Path
+from fastapi import FastAPI, status, Body
 
 app = FastAPI()
 
-## Auth
-@app.post(
-    path='/signup',
-    response_model=User,
-    status_code=status.HTTP_201_CREATED,
-    summary='Register an User',
-    tags=['Users']
-)
-def signup():
-    pass
-
-@app.post(
-    path='/login',
-    response_model=User,
-    status_code=status.HTTP_200_OK,
-    summary='Login an User',
-    tags=['Users']
-)
-def login():
-    pass
+# Path Operations
 
 ## Users
 
+### Register a user
+@app.post(
+    path="/signup",
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary="Register a User",
+    tags=["Users"]
+)
+def signup(user: UserRegister = Body(...)): 
+    """
+    Signup
+
+    This path operation register a user in the app
+
+    Parameters: 
+        - Request body parameter
+            - user: UserRegister
+    
+    Returns a json with the basic user information: 
+        - user_id: UUID
+        - email: Emailstr
+        - first_name: str
+        - last_name: str
+        - birth_date: datetime
+    """
+    with open("users.json", "r+", encoding="utf-8") as f: 
+        results = json.loads(f.read())
+        user_dict = user.dict()
+        user_dict["user_id"] = str(user_dict["user_id"])
+        user_dict["birth_date"] = str(user_dict["birth_date"])
+        results.append(user_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return user
+
+
+### Login a user
+@app.post(
+    path="/login",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Login a User",
+    tags=["Users"]
+)
+def login(): 
+    pass
+
+### Show all users
 @app.get(
-    path='/users',
+    path="/users",
     response_model=List[User],
     status_code=status.HTTP_200_OK,
-    summary='Show all users',
-    tags=['Users']
+    summary="Show all users",
+    tags=["Users"]
 )
-def show_all_users():
+def show_all_users(): 
     pass
 
+### Show a user
 @app.get(
-    path='/users/{user_id}',
+    path="/users/{user_id}",
     response_model=User,
     status_code=status.HTTP_200_OK,
-    summary='Show an User',
-    tags=['Users']
+    summary="Show a User",
+    tags=["Users"]
 )
-def show_an_user():
+def show_a_user(): 
     pass
 
+### Delete a user
 @app.delete(
-    path='/users/{user_id}/delete',
+    path="/users/{user_id}/delete",
     response_model=User,
     status_code=status.HTTP_200_OK,
-    summary='Delete an User',
-    tags=['Users']
+    summary="Delete a User",
+    tags=["Users"]
 )
-def delete_an_user():
+def delete_a_user(): 
     pass
 
+### Update a user
 @app.put(
-    path='/users/{user_id}/update',
+    path="/users/{user_id}/update",
     response_model=User,
     status_code=status.HTTP_200_OK,
-    summary='Update an User',
-    tags=['Users']
+    summary="Update a User",
+    tags=["Users"]
 )
-def update_an_user():
+def update_a_user(): 
     pass
+
 
 ## Tweets
 
@@ -84,8 +118,7 @@ def update_an_user():
     tags=["Tweets"]
 )
 def home():
-    # return {"Twitter API": "Working!"}
-    pass
+    return {"Twitter API": "Working!"}
 
 ### Post a tweet
 @app.post(
--- models.py
+++ models.py
@@ -1,60 +1,51 @@
 # Python
 from uuid import UUID
 from datetime import date, datetime
-from typing import Optional
+from typing import TypeVar, Generic, Dict, Optional, List
 
 # Pydantic
 from pydantic import BaseModel, Field, EmailStr
 
+# Models
+
 class UserBase(BaseModel):
-
-    id: UUID = Field(...,)
-
-    email: EmailStr = Field(...,)
-
-
-class User(UserBase):
-
-    first_name: str = Field(...,
-                            title='First name',
-                            min_length=2,
-                            max_length=50,
-                            example='John',)
-
-    last_name: str = Field(...,
-                           title='Last name',
-                           min_length=2,
-                           max_length=50,
-                           example='Doe',)
-
-    birth_date: Optional[date] = Field(default=None,
-                                       title='Birth date',
-                                       example='2021-01-01',)
-
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
 
 class UserLogin(UserBase):
+    password: str = Field(
+        ..., 
+        min_length=8,
+        max_length=64
+    )
 
-    password: str = Field(...,
-                          min_length=8,
-                          max_length=64,
-                          example='password',)
+class User(UserBase):
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    birth_date: Optional[date] = Field(default=None)
 
+class UserRegister(User):
+    password: str = Field(
+        ..., 
+        min_length=8,
+        max_length=64
+    )
 
 class Tweet(BaseModel):
-
-    id: UUID = Field(...)
-
-    content: str = Field(...,
-                         min_length=1,
-                         max_length=256,)
-
-    created_at: datetime = Field(default=datetime.now(),
-                                title='Creation date',
-                                example='2020-01-01T00:00:00Z',)
-
-    updated_at: Optional[datetime] = Field(default=None,
-                                           title='Last update date',
-                                           example='2020-01-01T00:00:00Z',)
-
-    created_by: User = Field(...,
-                             title='User who created the tweet',)
+    tweet_id: UUID = Field(...)
+    content: str = Field(
+        ..., 
+        min_length=1, 
+        max_length=256
+    )
+    created_at: datetime = Field(default=datetime.now())
+    update_at: Optional[datetime] = Field(default=None)
+    by: User = Field(...)
